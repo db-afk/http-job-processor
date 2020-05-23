136087ac0a251bf03c09c7f23f4c0b5b3efa6d08
Dobri Karadzhov <dobri.karadjov@gmail.com>
2020-05-23 23:55:47 +0300

    Initial commit
    - file structure
    - ignore file
    - task description as README.md

---
+++ src/main/java/com/dok/HttpJobProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.dok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpJobProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpJobProcessorApplication.class, args);
+    }
+}
